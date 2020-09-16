35aa0aa1e115956953d0c4d0cb23d0563813c83e
tube <43616130+chiseungii@users.noreply.github.com>
2020-09-17 05:06:25 +0900

    Create 0001.cpp

---
+++ 0001.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& num, int target) {
+        vector<int> result;
+		for (int i = 0; i < num.size() - 1; i++){
+			for (int j = i + 1; j < num.size(); j++){
+				if (num[i] + num[j] == target){
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+        }
+        
+        return result;
+	}
+};
